b93ba952c70edb9cc1d87aa6dc88cb61ec61e987
Tornike Zhizhiashvili <t.zhizhiashvili@adjarabet.com>
2022-05-20 00:35:31 +0400

    add color slider

--- my-app/src/ColorProject/ColorComponents/Palette.js
+++ my-app/src/ColorProject/ColorComponents/Palette.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ColorBox from './ColorBox';
 import "../ColorCss/Palette.css";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css"
 
 
-const Palette = (props) => {
+const Palette = ({palette}) => {
 
-    const colorBoxes = props.colors.map(color=>(
-        <ColorBox background={color.color} name={color.name} key={color.name}></ColorBox>
+    const [level, setLever] = useState(500);
+
+
+    const changeLevel = (level) => {
+
+        setLever(level)
+    }
+
+    const colorBoxes = palette.colors[level].map(color=>(
+        <ColorBox background={color.hex} name={color.name} key={color.name}></ColorBox>
     ))
 
     return (
         <div className='Palette'>
+            <Slider defaultValue={level} min={100} max={900} step={100} onAfterChange={changeLevel}/>
             <div className='Palette-colors'>
-                     {colorBoxes}
+                    {colorBoxes}
             </div>
         </div>
     );
--- my-app/src/ColorProject/PaletteApp.js
+++ my-app/src/ColorProject/PaletteApp.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Palette from './ColorComponents/Palette';
 import SeedColors from './SeedColors';
-
+import {generatePalette} from "./ColorComponents/ColorHelers"
 
 const PaletteApp = () => {
+    console.log(generatePalette(SeedColors[4]))
     return (
         <>
             
-            <Palette {...SeedColors[4]} />
+            <Palette palette={generatePalette(SeedColors[4])} />
             
         </>
     );
